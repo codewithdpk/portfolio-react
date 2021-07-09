3c7f02fb41d8d448dda77aa51b427cb43d1d9bd5
codewithdpk <codewithdeepak@yahoo.com>
2021-07-09 18:27:56 +0530

    Initial update.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import LandingPage from './Pages/LandingPage';
+import { createBrowserHistory } from 'history';
 function App() {
+  const histroy = createBrowserHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router history = {histroy}>
+     <Switch>
+       <Route path="/" component ={LandingPage} exact={true}/>
+     </Switch>
+   </Router>
   );
 }
 
